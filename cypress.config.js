6a7e415522f25f790b2440cd3d10c29551467489
James <galavantinggeckoguy@gmail.com>
2024-03-19 19:56:54 -0700

    Added cypress video and screenshot artifact saving

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:8080/"
+    baseUrl: "http://localhost:8080/",
+    video: true,
+    videoUploadOnPasses: true,
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
   },
 });
-
